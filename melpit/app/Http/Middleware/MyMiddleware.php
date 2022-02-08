1714729cf41f790766a18b97478b4163b0c85c24
github newstage75 <rightkingdxanntume@gmail.com>
2022-02-08 22:14:50 +0900

    MyMiddlewareの変更（ミドルウェアの実行タイミングの確認）

--- melpit/app/Http/Middleware/MyMiddleware.php
+++ melpit/app/Http/Middleware/MyMiddleware.php
@@ -16,15 +16,29 @@ class MyMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $id = rand(0, count(MyService::alldata()));
-        MyService::setId($id);
-        $merge_data = [
-          'id' => $id,
-          'msg'=> MyService::say(),
+      //●before処理・開始
+      $id = rand(0, count(MyService::alldata()));
+      MyService::setId($id);
+      $merge_data = [
+          'id'     =>$id,
+          'msg'    =>MyService::say(),
           'alldata'=>MyService::alldata()
-        ];
-        $request->merge($merge_data);
+      ];
+      $request->merge($merge_data);
 
-        return $next($request);
+      //●before処理・終了
+      $response = $next($request);
+
+      //●after処理・開始
+      $content = $response->content();
+      $content .= '<style>
+          body {background-color:#eef;}
+          p {font-size:18pt;}
+          li {color:red; font-weight: bold;}
+        </style>';
+      $response->setContent($content);
+      //●after処理・終了
+
+      return $response;
     }
 }
